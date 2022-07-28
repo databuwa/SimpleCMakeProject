a1c0bb84133f9d1b078f9b2f5988b850bf3c4bcf
Dattha Buwaneka <bmxn43@zebra.com>
2022-07-28 13:55:00 +0530

    Add project files.

---
+++ SimpleCMakeProject/SimpleCMakeProject.cpp
@@ -0,0 +1,12 @@
+﻿// SimpleCMakeProject.cpp : Defines the entry point for the application.
+//
+
+#include "SimpleCMakeProject.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
